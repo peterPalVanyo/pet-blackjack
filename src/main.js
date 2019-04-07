b99dd0a643a41d7e21bc5d3be5767e76d288c05c
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-04-07 16:45:38 +0200

    createDeck and getNextCard functions

--- src/main.js
+++ src/main.js
@@ -5,15 +5,26 @@ card game first attempt to pet project
 let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"];
 
-let deck = [];
-
-
-for (let idxSuits = 0; idxSuits < suits.length; idxSuits++) {
-    for (let idxValues = 0; idxValues < values.length; idxValues++) {
-        deck.push(values[idxValues]  + " of " + suits[idxSuits]);
+function createDeck() {
+    let deck = [];
+    for (let idxSuits = 0; idxSuits < suits.length; idxSuits++) {
+        for (let idxValues = 0; idxValues < values.length; idxValues++) {
+            deck.push(values[idxValues]  + " of " + suits[idxSuits]);
+        }
     }
+    return deck;
 }
 
-for(let i = 0; i < deck.length; i++){
-    console.log(deck[i]);
+function getNextCard(){
+    return deck.shift();
 }
+
+let deck = createDeck();
+
+// for(let i = 0; i < deck.length; i++){
+//     console.log(deck[i]);
+// }
+
+let playerCards = [getNextCard(), getNextCard()];
+
+console.log(playerCards);
