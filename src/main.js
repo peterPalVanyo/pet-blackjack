d3b02966bfb39374fd5733c675c8efe197fefa4a
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-04-07 18:25:37 +0200

    setup the html basics: three button and the texts

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ card game first attempt to pet project
 let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"];
 
+let textArea = document.getElementById(`text-area`);
+let newGameButton = document.getElementById(`new-game-button`);
+let hitButton = document.getElementById(`hit-button`);
+let stayButton = document.getElementById(`stay-button`);
+
+hitButton.style.display = `none`;
+stayButton.style.display = `none`;
+newGameButton.addEventListener(`click`, function () {
+    textArea.innerText = `Let us start the game....`;
+    newGameButton.style.display = `none`;
+    hitButton.style.display = `inline`;
+    stayButton.style.display = `inline`;
+});
+
 function createDeck() {
     let deck = [];
     for (let idxSuits = 0; idxSuits < suits.length; idxSuits++) {
