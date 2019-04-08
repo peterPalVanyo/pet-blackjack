fcd108d538d853688fc356bcbe946c761f9bb2cf
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-04-08 06:24:01 +0200

    fix the object display problem (just hard refresh), start the hit and stay button functions

--- src/main.js
+++ src/main.js
@@ -61,43 +61,6 @@ function getCardString(card) {
     return card.value + " of " + card.suit;
 }
 
-function showStatus() {
-    if (!gameStarted) {
-        textArea.innerText = "Welcome to Blackjack";
-        return;
-    }
-    let dealerCardString = "";
-    for(let i = 0; i < dealerCards.length; i++){
-        dealerCardString += dealerCards[i] + `\n`;
-    }
-    let playerCardString = "";
-    for(let i = 0; i < playerCards.length; i++){
-        playerCardString += playerCards[i] + `\n`;
-    }
-
-    updateScore();
-
-    textArea.innerText =
-        "Dealer has:\n " +
-        dealerCardString +
-        "(score " + dealerScore + ")\n\n" +
-
-        "Player has:\n " +
-        playerCardString +
-        "(score " + playerScore + ")\n\n";
-
-    if(gameOver){
-        if(playerWon){
-            textArea.innerText += "YOU WIN!";
-        } else{
-            textArea.innerText += "THE DEALER WINS!";
-        }
-        newGameButton.style.display = `inline`;
-        hitButton.style.display = `none`;
-        stayButton.style.display = `none`;
-    }
-}
-
 
 function getNextCard() {
     return deck.shift();
@@ -149,7 +112,49 @@ function updateScore(){
     playerScore = getScore(playerCards);
 }
 
-//actions
+function checkForEndOfGame() {
+    
+}
+
+
+function showStatus() {
+    if (!gameStarted) {
+        textArea.innerText = "Welcome to Blackjack";
+        return;
+    }
+    let dealerCardString = "";
+    for(let i = 0; i < dealerCards.length; i++){
+        dealerCardString += getCardString(dealerCards[i]) + `\n`;
+    }
+    let playerCardString = "";
+    for(let i = 0; i < playerCards.length; i++){
+        playerCardString += getCardString(playerCards[i]) + `\n`;
+    }
+
+    updateScore();
+
+    textArea.innerText =
+        "Dealer has:\n " +
+        dealerCardString +
+        "(score " + dealerScore + ")\n\n" +
+
+        "Player has:\n " +
+        playerCardString +
+        "(score " + playerScore + ")\n\n";
+
+    if(gameOver){
+        if(playerWon){
+            textArea.innerText += "YOU WIN!";
+        } else{
+            textArea.innerText += "THE DEALER WINS!";
+        }
+        newGameButton.style.display = `inline`;
+        hitButton.style.display = `none`;
+        stayButton.style.display = `none`;
+    }
+}
+
+//actions (listeners)
 
 newGameButton.addEventListener(`click`, function () {
     gameStarted = true;
@@ -168,9 +173,18 @@ newGameButton.addEventListener(`click`, function () {
     showStatus();
 });
 
-// for(let i = 0; i < deck.length; i++){
-//     console.log(deck[i]);
-// }
+hitButton.addEventListener(`click`, function () {
+   playerCards.push(getNextCard());
+   checkForEndOfGame();
+   showStatus();
+});
+
+stayButton.addEventListener(`click`, function () {
+   gameOver = true;
+   checkForEndOfGame();
+   showStatus();
+});
+
 
 
 
